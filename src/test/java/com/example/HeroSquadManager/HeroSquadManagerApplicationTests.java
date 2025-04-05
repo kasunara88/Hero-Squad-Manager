ad18b86314e919d3c471de1e0ca0b33374347f4c
kasunara88 <wmaravinda@gmail.com>
2025-04-05 12:59:21 +0530

    Initial commit

---
+++ src/test/java/com/example/HeroSquadManager/HeroSquadManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.HeroSquadManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeroSquadManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
